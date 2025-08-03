fa02086af85f4ea6ac04e81929c6a784fbcac793
lukasmateju <153325507+lukasmateju@users.noreply.github.com>
2025-08-03 00:01:35 +0000

    Deploying to gh-pages from @ lukasmateju/lukasmateju.github.io@91518e17ce035dd48e0d30362aec394aeb39d9e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-resume",
           title: "Resume",
-          description: "",
+          description: "View and download Lukas Mateju&#39;s latest resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
